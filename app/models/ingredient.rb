bd206806d429933de8bd0584395e698df57e2fd6
emalios <hugo.bergerat2002@gmail.com>
2022-11-10 18:53:52 +0100

    add alcohol column

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ApplicationRecord
 
   scope :lower_than, ->(amount) {where('price < ?', amount)}
   scope :higher_than, ->(amount) {where('price > ?', amount)}
-  scope :is_alcohol, -> {where('alcohol = true')}
+  scope :is_alcohol, -> {where alcohol: true}
 
   validates :name, presence: true
   validates :price, presence: true
