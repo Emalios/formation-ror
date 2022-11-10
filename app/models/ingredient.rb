a8db735e13fb8e96a044854bb8bd0a845e4819c0
emalios <hugo.bergerat2002@gmail.com>
2022-11-10 18:47:51 +0100

    add scopes and validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,11 @@
 class Ingredient < ApplicationRecord
+
+  scope :lower_than, ->(amount) {where('price < ?', amount)}
+  scope :higher_than, ->(amount) {where('price > ?', amount)}
+  scope :is_alcohol, -> {where('alcohol = true')}
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, comparison: {greater_than: 0.0}
+
 end
